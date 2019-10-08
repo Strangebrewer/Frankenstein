c8761f812cd854ecea59ee2f2f7f948a0855596f
Strangebrewer <BKAShambala@gmail.com>
2019-10-07 21:38:48 -0600

    adding NODE_ENV conditional to server.js

--- server.js
+++ server.js
@@ -11,11 +11,15 @@ app.use(bodyParser.json());
 app.use(bodyParser.text());
 
 app.use((req, res, next) => {
-   res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+   res.header("Access-Control-Allow-Origin", "*");
    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Authorization, Accept");
    res.header("Access-Control-Allow-Methods", "*");
    next();
 });
+
+if (process.env.NODE_ENV === 'production') {
+   app.use(express.static('client/build'))
+}
 
 app.use(routes);
 
