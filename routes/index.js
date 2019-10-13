8c82d824fe5d0f134ac2aa107454e4560ec889bc
Strangebrewer <BKAShambala@gmail.com>
2019-10-13 04:32:40 -0600

    converting client to create-react-app

--- routes/index.js
+++ routes/index.js
@@ -8,6 +8,13 @@ router.use('/users', user_routes);
 router.use('/dragons', dragon_routes);
 
 // If no API routes are hit, send the React app
+// Without this, if you refresh the page on anything but the base url (i.e. "/")
+// It will respond with a 'Cannot GET' error because the server
+//    is trying to respond to that url from the back end
+//    rather than letting react-router handle it on the front end
+// So this ensures the routing will work properly;
+//    it is important for when a user either types the url themselves
+//    or saves the page as one of the routes rather than the base url.
 router.use(function (req, res) {
   res.sendFile(path.join(__dirname, "../client/build/index.html"));
 });
