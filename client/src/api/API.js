c51f1dbfb200b5e6ec52aac560c8da8b232b1a82
Strangebrewer <BKAShambala@gmail.com>
2019-10-13 22:07:57 -0600

    adjust base url

--- client/src/api/API.js
+++ client/src/api/API.js
@@ -1,10 +1,7 @@
 import axios from 'axios'
 
-const { REACT_APP_NODE_ENV } = process.env;
-let BASE_URL = 'http://localhost:3001';
-if (REACT_APP_NODE_ENV === 'production') {
-   BASE_URL = 'https://bkashambhala.com';
-}
+// let BASE_URL = 'http://localhost:3001';
+let BASE_URL = 'https://bkashambhala.com';
 
 export default () => {
    return axios.create({
